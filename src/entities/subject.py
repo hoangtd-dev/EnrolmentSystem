4c47c9537eb27a94024370467f77e0b7c5e0f20c
duy hoang <hoangtd.dev@gmail.com>
2024-10-30 22:18:09 +1100

    remove redundant entities following by requirement

--- src/entities/subject.py
+++ src/entities/subject.py
@@ -1,11 +1,37 @@
+from ..enums.grade_type_enum import GradeTypeEnum
+
 class Subject:
-    def __init__(self, id):
+    def __init__(self, id, mark, grade=None):
         self.__id = id
+        self.__mark = mark
+        self.__grade = grade if grade else self.get_classify_grade(mark)
 
     def get_id(self):
         return self.__id
 
+    def get_mark(self):
+        return self.__mark
+
+    def get_grade(self):
+        return self.__grade
+
+    @staticmethod
+    def get_classify_grade(mark):
+        """Classifies the grade type based on the mark."""
+        if mark >= 85:
+            return GradeTypeEnum.HIGH_DISTINCTION
+        elif mark >= 75:
+            return GradeTypeEnum.DISTINCTION
+        elif mark >= 65:
+            return GradeTypeEnum.CREDIT
+        elif mark >= 50:
+            return GradeTypeEnum.PASS
+        else:
+            return GradeTypeEnum.FAIL
+
     def to_dict(self):
         return {
-            "id": self.__id
+            "id": self.__id,
+            "mark": self.__mark,
+			"type": self.__grade
         }
